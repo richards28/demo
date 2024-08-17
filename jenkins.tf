519a05c2be34e471523ff8055f3e814c0eda84c1
richards28 <richirocks96@gmail.com>
2024-08-17 11:53:48 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -1,7 +1,7 @@
 #ec2 private
 resource "aws_instance" "dev2" {
     count = 3
-    ami = "ami-03350e4f182961c7f"
+    ami = "ami-0091f05e4b8ee6709"
     instance_type = "t2.micro"
     key_name = "keypair1"
     tags= {
